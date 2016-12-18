78f841054b272739936e7dbb0bd6064ef73285c4
maxwell miller <maxwell miller>
2016-12-18 12:17:01 +0800

    b

--- server.py
+++ server.py
@@ -48,8 +48,9 @@ class FileFolderHandler(tornado.web.RequestHandler):
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+    	self.request.remote_ip = self.headers['X-Real-Ip']
         if self.request.remote_ip not in clients['ips']:
-            print("%s linked to host"%self.request.remote_ip)
+            print("%s linked to host%s"%self.request.remote_ip)
             clients['ips'].add(self.request.remote_ip)
             clients[self.request.remote_ip] = queue.Queue(50)
         else:
